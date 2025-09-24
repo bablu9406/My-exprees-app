7560aeb58ad7baf252c57ee558c972d08d82f5fa
bablu9406 <krishnabablu22@gmail.com>
2025-09-24 20:40:52 +0530

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   text: String,
   createdAt: { type: Date, default: Date.now }
 });
 
 const postSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
   caption: { type: String, default: '' },
   imageUrl: { type: String, default: '' }, // client uploads image to Cloudinary/Firebase and sends URL
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
   comments: [commentSchema]
 }, { timestamps: true });
 
