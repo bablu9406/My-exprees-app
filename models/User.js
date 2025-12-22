baebbdba70153c209828f22bbc26e9fbd480017f
bablu9406 <krishnabablu22@gmail.com>
2025-12-22 15:42:05 +0530

    Refactor User model to use ES6 imports and update schema

--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String }, // hashed
-  profilePic: { type: String },
-  bio: { type: String, default: '' },
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
-}, { timestamps: true });
+  username: String,
+  email: String,
+  password: String,
 
-module.exports = mongoose.model('user', userSchema);
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+export default mongoose.model("User", userSchema);
