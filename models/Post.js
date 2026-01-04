db053bd13af17992275bfb6bfbf121ab407aa7b0
bablu9406 <krishnabablu22@gmail.com>
2026-01-04 14:43:47 +0530

    Change import/export syntax to CommonJS in Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     caption: String,
     image: String,
-
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
 
-export default mongoose.model("Post", postSchema);
+module.exports = mongoose.model("Post", postSchema);
