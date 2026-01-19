08967e1576b60355c0c04761ad2ca2d652ea8ff3
bablu9406 <krishnabablu22@gmail.com>
2026-01-19 20:58:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/cginsta")
-  .then(() => {
     console.log("MongoDB connected");
     app.listen(5000, "0.0.0.0", () => {
       console.log("Server running on port 5000");
